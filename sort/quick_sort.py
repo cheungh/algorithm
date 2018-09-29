305b2d2d593b2dd260dbc049a29b16e20661c170
Howard Cheung <31749983+cheungh@users.noreply.github.com>
2018-09-29 15:49:08 -0700

    Update quick_sort.py

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,34 +1,46 @@
 # randomized pivot to prevent O(n2)
 from random import randint
 
-def QuickSort(A, start, end):
-    # base case
-    # print "run qs [%s] [%s]" % (start, end)
-    if end > start:
-        pivot = partition(A, start, end)
-        QuickSort(A, start, pivot - 1)
-        QuickSort(A, pivot + 1, end)
+from random import randint
 
-def partition(A, start, end):
-    # print "run partition [%s] [%s]" % (start, end)
-    # print "A is  [%s]" % A
-    # pivot = end
-    # pivot = randint(start, end)
-    # pivot = start
-    pivot = ((end - start) / 2) + start
-    # print "pivot is %s" % pivot
-    A[pivot], A[end] = A[end], A[pivot]
-    i = start - 1
-    for j in xrange(start, end):
-        if A[j] <= A[end]:
+def partition(arr, low, high):
+    # 1. select a pivot algorithm
+    # print(arr)
+    pivot_value = arr[high]
+    # let i be smaller index, initialize with low - 1
+    i = low - 1
+    # for j from 0 to high - 1
+    for j in range(low, high):
+        # if arr[j] <= pivot
+        #   then
+        #   1. increment the small index
+        #   thus shifting the pivot point
+        #   2. swap arr[i] with arr[j], the current and smaller index
+        if arr[j] <= pivot_value:
+            # swap the pivot with the last smaller index + 1
+            # this is the pivot position
             i += 1
-            A[i], A[j] = A[j], A[i]
-    A[i+1], A[end] = A[end], A[i+1]
-    # print "p %s : A is [%s]" % (i + 1, A)
+            arr[i], arr[j] = arr[j], arr[i]
+            # return the pivot position which is smaller index + 1
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    # print(arr)
     return i + 1
 
 
+def quick_sort(arr, low, high):
 
+    # base case
+    if low < high: # otherwise finish
+        pivot = partition(arr, low, high)
+
+        # divide conquer the left
+        quick_sort(arr, low, pivot - 1)
+
+        # divide conquer the right
+        quick_sort(arr, pivot + 1, high)
+
+if __name__ == '__main__':
+    
 # X = [4, 13, 8, 3, 1, 2]
 X = []
 end = 2000001
